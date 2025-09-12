f041f96558a1d436639b87eb5ba8186b11499b63
Paulo Fernando <74271792+Ppaulofernando@users.noreply.github.com>
2025-09-12 13:17:32 -0300

    Update Logica_dio.java

--- src/Projeto_logica_DIO/Logica_dio.java
+++ src/Projeto_logica_DIO/Logica_dio.java
@@ -1,15 +1,15 @@
 package Projeto_logica_DIO;
 
 /*
- Projeto DIO-Savegnago LÛgica De ProgramaÁ„o
+ Projeto DIO-Savegnago L√≥gica De Programa√ß√£o
 O que dever ser utilizado :
--Vari·veis
+-Vari√°veis
 -Operadores
--LaÁos De RepetiÁ„o
--Estrutura De Decisıes
+-La√ßos De Repeti√ß√£o
+-Estrutura De Decis√µes
      Objetivo:
-Criar uma vari·vel para armazenar o nome e a quantidade de experiÍncia(XP) de um herÛi,
-depois utilize uma estrutura de decis„o para apresentar alguma das mensagens abaixo :
+Criar uma vari√°vel para armazenar o nome e a quantidade de experi√™ncia(XP) de um her√≥i,
+depois utilize uma estrutura de decis√£o para apresentar alguma das mensagens abaixo :
 Se XP for menor do que 1.000      = Ferro
 Se XP for entre 1.001 e 2.000     = Bronze
 Se XP for entre 2.001 e 6.000     = Prata
@@ -18,9 +18,9 @@ Se XP for entre 7.001 e 8.000     = Platina
 Se XP for entre 8.001 e 9.000     = Ascendente
 Se XP for entre 9.001 e 10.000    = Imortal 
 Se XP for maior ou igual a 10.001 = Radiante
-## SaÌda
+## Sa√≠da
 Ao final deve exibir a seguinte mensagem :
-"O herÛi de nome"**{Nome}** est· no nÌvel de **{nÌvel}**
+"O her√≥i de nome"**{Nome}** est√° no n√≠vel de **{n√≠vel}**
 @author Paulo Fernando
  */
 
@@ -31,7 +31,7 @@ public class Logica_dio {
 		String nome = "Shieldguy";
 		int multi;
 		int nivel = 0;
-
+		// Pegando um n√∫mero aleat√≥rio do computador atrav√©s da fun√ß√£o Manth.random()
 		multi = (int) (Math.random() * 11 + 1);
 
 		for (int i = 1; i <= multi; i++) {
@@ -39,21 +39,21 @@ public class Logica_dio {
 		}
 		nivel = nivel * 1000;
 		if (nivel <= 1000) {
-			System.out.println("O herÛi de nome " + nome + " est· no nÌvel de Ferro");
+			System.out.println("O her√≥i de nome " + nome + " est√° no n√≠vel de Ferro");
 		} else if (nivel >= 1001 && nivel <= 2000) {
-			System.out.println("O herÛi de nome " + nome + " est· no nÌvel de Bronze");
+			System.out.println("O her√≥i de nome " + nome + " est√° no n√≠vel de Bronze");
 		} else if (nivel >= 2001 && nivel <= 6000) {
-			System.out.println("O herÛi de nome " + nome + " est· no nÌvel de Prata");
+			System.out.println("O her√≥i de nome " + nome + " est√° no n√≠vel de Prata");
 		} else if (nivel >= 6001 && nivel <= 7000) {
-			System.out.println("O herÛi de nome " + nome + " est· no nÌvel de Ouro");
+			System.out.println("O her√≥i de nome " + nome + " est√° no n√≠vel de Ouro");
 		} else if (nivel >= 7001 && nivel <= 8000) {
-			System.out.println("O herÛi de nome " + nome + " est· no nÌvel de Platina");
+			System.out.println("O her√≥i de nome " + nome + " est√° no n√≠vel de Platina");
 		} else if (nivel >= 8001 && nivel <= 9000) {
-			System.out.println("O herÛi de nome " + nome + " est· no nÌvel de Ascendente");
+			System.out.println("O her√≥i de nome " + nome + " est√° no n√≠vel de Ascendente");
 		} else if (nivel >= 9001 && nivel <= 10000) {
-			System.out.println("O herÛi de nome " + nome + " est· no nÌvel de Imortal");
+			System.out.println("O her√≥i de nome " + nome + " est√° no n√≠vel de Imortal");
 		} else {
-			System.out.println("O herÛi de nome " + nome + " est· no nÌvel de Radiante");
+			System.out.println("O her√≥i de nome " + nome + " est√° no n√≠vel de Radiante");
 		}
 	}
 }
